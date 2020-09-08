6d3a6ff03589559597739034e75dc3f23766ca70
moritz <moritz.hueper@posteo.net>
2020-09-08 18:50:33 +0200

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SimpleLineChart from 'components/SimpleLineChart/SimpleLineChart';
+import SynchronizedLineChart from 'components/SynchronizedLineChart/SynchronizedLineChart';
+import SimpleBarChart from 'components/SimpleBarChart/SimpleBarChart';
+import BrushBarChart from "components/BrushBarChart/BrushBarChart";
+import CustomActiveShapePieChart from "components/CustomActiveShapePieChart/CustomActiveShapePieChart";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SimpleLineChart />
+        <SynchronizedLineChart />
+        <SimpleBarChart />
+        <BrushBarChart />
+        <CustomActiveShapePieChart />
       </header>
     </div>
   );
